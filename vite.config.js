c5a6114c0aea7bb55fe7cf0893f58b836d5195a1
jhojanCT <jhojancoro1@gmail.com>
2026-04-14 21:38:47 -0400

    Actualizo vite.config.js para producción

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    base: '/',                    // ← Muy importante
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+    },
 });
